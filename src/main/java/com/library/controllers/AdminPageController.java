a79d0c76dadbeda6dfffdcfd096e509e5935d90c
ThufirHawat13 <kurnyshov1998@gmail.com>
2022-11-21 15:33:53 +0300

    init

---
+++ src/main/java/com/library/controllers/AdminPageController.java
@@ -0,0 +1,14 @@
+package com.library.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminPageController {
+
+    @GetMapping("/admin")
+    public String getAdminPage() {
+        return "/admin/adminPage";
+    }
+}
