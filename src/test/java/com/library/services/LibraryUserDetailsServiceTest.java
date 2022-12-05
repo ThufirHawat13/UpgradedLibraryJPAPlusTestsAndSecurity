87c9b7e0c6bea81bf1d0d654c7cb4ac1eb80559d
ThufirHawat13 <kurnyshov1998@gmail.com>
2022-12-05 16:24:34 +0300

    init

--- src/test/java/com/library/services/LibraryUserDetailsServiceTest.java
+++ src/test/java/com/library/services/LibraryUserDetailsServiceTest.java
@@ -4,41 +4,47 @@ import com.library.models.LibraryUser;
 import com.library.repositories.LibraryUserRepository;
 import com.library.security.LibraryUserDetails;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class LibraryUserDetailsServiceTest {
 
-    private final LibraryUserDetailsService libraryUserDetailsService;
+  private final LibraryUserDetailsService libraryUserDetailsService;
 
-    @Autowired
-    LibraryUserDetailsServiceTest(LibraryUserDetailsService libraryUserDetailsService) {
-        this.libraryUserDetailsService = libraryUserDetailsService;
-    }
+  @Autowired
+  LibraryUserDetailsServiceTest(LibraryUserDetailsService libraryUserDetailsService) {
+    this.libraryUserDetailsService = libraryUserDetailsService;
+  }
 
-    @MockBean
-    private LibraryUserRepository libraryUserRepository;
+  @MockBean
+  private LibraryUserRepository libraryUserRepository;
 
-    @Mock
-    private LibraryUser libraryUser;
+  @Mock
+  private LibraryUser libraryUser;
 
-    @Mock
-    private UserDetails userDetails;
+  @Mock
+  private UserDetails userDetails;
 
-    @Test
-    void loadUserByUsername() {
-        String username = "n";
-        Mockito.when(libraryUserDetailsService.loadUserByUsername(username)).thenReturn(userDetails);
-        Mockito.when(libraryUserRepository.findByUsername(username)).thenReturn(Optional.of(new LibraryUser("testUser", "testUser")));
-        userDetails = libraryUserDetailsService.loadUserByUsername(username);
+  @Test
+  void loadUserByUsername() {
+    String username = "n";
+    Mockito.when(libraryUserRepository.findByUsername(username))
+        .thenReturn(Optional.of(libraryUser));
+    userDetails = libraryUserDetailsService.loadUserByUsername(username);
 
-        Mockito.verify(libraryUserDetailsService, Mockito.times(1)).loadUserByUsername(username);
-    }
+    Mockito.verify(libraryUserRepository, Mockito.times(1))
+        .findByUsername(username);
+  }
 }
